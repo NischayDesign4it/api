737ed659c9c515ea0d284b37effa739a123e6cb0
NischayDesigns4it <nischayv@designi4t.com>
2024-05-10 13:22:30 +0530

    initial

--- dhtDwin/dht/models.py
+++ dhtDwin/dht/models.py
@@ -5,8 +5,8 @@ from django.db import models
 
 
 class temphum(models.Model):
-    temperature = models.DecimalField(max_digits=10, decimal_places=2)
-    humidity = models.DecimalField(max_digits=10, decimal_places=2)
+    temperature = models.DecimalField(max_digits=20, decimal_places=10)
+    humidity = models.DecimalField(max_digits=20, decimal_places=10)
     timestamp = models.DateTimeField(auto_now_add=True, auto_now=False, blank=True)
 
     def __str__(self):
