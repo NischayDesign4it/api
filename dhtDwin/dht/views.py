5bd004d5393649f4c287882cefae52bd4317b6f5
NischayDesigns4it <nischayv@designi4t.com>
2024-05-10 13:24:33 +0530

    initial

--- dhtDwin/dht/views.py
+++ dhtDwin/dht/views.py
@@ -9,7 +9,7 @@ class TempHumCreateAPIView(APIView):
         serializer = TempHumSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_CREATED)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class TempHumListAPIView(APIView):
